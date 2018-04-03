e52b04d567b6bf3af225b19df2849b6cb3b99d61
JokerAlex <alex.zhao1023@gmail.com>
2018-04-03 16:02:57 +0800

    excel文件读取功能完善

--- src/main/java/com/grades/utils/ExcelReader.java
+++ src/main/java/com/grades/utils/ExcelReader.java
@@ -1,28 +1,48 @@
 package com.grades.utils;
 
 
-import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFDateUtil;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.DateUtil;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.util.StringUtils;
-
-import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ExcelReader {
 
-    public static Workbook getWb(String path) {
-        try {
-            return WorkbookFactory.create(new File(path));
-        } catch (Exception e) {
-            throw new RuntimeException("读取EXCEL文件出错", e);
+    private final static String excel2003L =".xls";    //2003- 版本的excel
+    private final static String excel2007U =".xlsx";   //2007+ 版本的excel
+
+    public static Workbook getWorkbook(String fileName) throws Exception{
+        Workbook wb = null;
+        InputStream inStr = new FileInputStream(fileName);
+        String fileType = fileName.substring(fileName.lastIndexOf("."));
+        if(excel2003L.equals(fileType)){
+            wb = new HSSFWorkbook(inStr);  //2003-
+        }else if(excel2007U.equals(fileType)){
+            wb = new XSSFWorkbook(inStr);  //2007+
+        }else{
+            throw new Exception("解析的文件格式有误！");
         }
+        return wb;
     }
 
-    public static String getCellValue(HSSFCell cell) {
+    public static Sheet getSheet(Workbook wb,int sheetIndex){
+        if (wb == null){
+            throw new RuntimeException("工作簿为空");
+        }
+        int sheetSize = wb.getNumberOfSheets();
+        if (sheetIndex < 0 || sheetIndex > sheetSize){
+            throw new RuntimeException("工作表获取错误");
+        }
+        return wb.getSheetAt(sheetIndex);
+    }
+
+    public static String getCellValue(Cell cell) {
         String cellValue = "";
         if (cell == null){
             cellValue = "null";
@@ -57,5 +77,36 @@ public class ExcelReader {
           }
         }
         return cellValue;
+    }
+
+    public static List<List<String>> getExcelRows(Sheet sheet, int startLine, int endLine){
+        List<List<String>> listResult = new ArrayList<List<String>>();
+        // 如果开始行号和结束行号都是-1的话，则全表读取
+        if (startLine == -1)
+            startLine = 0;
+        if (endLine == -1) {
+            endLine = sheet.getLastRowNum() + 1;
+        } else {
+            endLine += 1;
+        }
+
+        for (int i = startLine; i < endLine; i++) {
+            Row row = sheet.getRow(i);
+            if (row == null){
+                //空行直接跳过
+                continue;
+            }
+            //获取列数
+            int rowSize = row.getLastCellNum();
+            //接受每一行的结果
+            List<String> rowList = new ArrayList<String>();
+            for (int j = 0; j < rowSize; j++) {
+                Cell cell = row.getCell(j);
+                String temp = getCellValue(cell);
+                rowList.add(j,temp);
+            }
+            listResult.add(i,rowList);
+        }
+        return listResult;
     }
 }
