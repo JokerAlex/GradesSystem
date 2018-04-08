e2744ce7cf250e22c6d8bf0cfa2afa79c0767966
JokerAlex <alex.zhao1023@gmail.com>
2018-04-08 20:14:58 +0800

    学院信息维护

--- src/main/java/com/grades/service/MaintainService.java
+++ src/main/java/com/grades/service/MaintainService.java
@@ -7,4 +7,8 @@ public interface MaintainService {
     String changPwd(String userId, String oldPwd, String newPwd);
 
     boolean updateUserInfo(User user);
+
+    String delCollege(String collegeId);
+
+    String updateCollege(String collegeId, String collegeName, String collegeIdOld);
 }
--- src/main/java/com/grades/serviceImpl/MaintainServiceImpl.java
+++ src/main/java/com/grades/serviceImpl/MaintainServiceImpl.java
@@ -1,5 +1,6 @@
 package com.grades.serviceImpl;
 
+import com.grades.mapping.CollegeMapper;
 import com.grades.mapping.UserMapper;
 import com.grades.model.User;
 import com.grades.service.MaintainService;
@@ -11,12 +12,15 @@ import org.springframework.stereotype.Service;
 public class MaintainServiceImpl implements MaintainService {
 
     private UserMapper userMapper;
+    private CollegeMapper collegeMapper;
 
     @Autowired
-    public MaintainServiceImpl(UserMapper userMapper){
+    public MaintainServiceImpl(UserMapper userMapper, CollegeMapper collegeMapper){
         this.userMapper = userMapper;
+        this.collegeMapper = collegeMapper;
     }
 
+    //修改密码
     public String changPwd(String userId, String oldPwd, String newPwd) {
         if ((userId != null && !userId.equals(""))
                 && (oldPwd != null && !oldPwd.equals(""))
@@ -30,6 +34,7 @@ public class MaintainServiceImpl implements MaintainService {
         return "{\"oldPwd\":\"false\",\"changeResult\":\"false\"}";
     }
 
+    //用户信息更新
     public boolean updateUserInfo(User user) {
         if (user == null){
             return false;
@@ -41,5 +46,27 @@ public class MaintainServiceImpl implements MaintainService {
         temp.setGrade(user.getGrade());
         boolean flag = userMapper.updateUserInfo(temp);
         return flag;
+    }
+
+    //学院信息删除
+    public String delCollege(String collegeId) {
+        boolean delResult = false;
+        if (collegeId !=null && !collegeId.trim().equals("")){
+            delResult = collegeMapper.delCollege(Integer.valueOf(collegeId));
+        }
+        return "{\"delResult\":\""+delResult+"\"}";
+    }
+
+    //学院信息更新
+    public String updateCollege(String collegeId, String collegeName, String collegeIdOld) {
+        boolean isCollegeId = collegeId != null && !collegeId.trim().equals("");
+        boolean isCollegeName = collegeName != null && !collegeName.trim().equals("");
+        boolean updateResult = false;
+        if (isCollegeId || isCollegeName) {
+            int id = Integer.valueOf(collegeId);
+            int idOld = Integer.valueOf(collegeIdOld);
+            updateResult = collegeMapper.updateCollege(id,collegeName,idOld);
+        }
+        return "{\"updateResult\":\""+updateResult+"\"}";
     }
 }
