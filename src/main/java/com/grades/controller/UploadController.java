fdb7b5ce6e50997774cf4f06bb1aeb4f2dda5407
JokerAlex <alex.zhao1023@gmail.com>
2018-04-19 15:13:10 +0800

    文件上传与写入数据库

--- src/main/java/com/grades/controller/UploadController.java
+++ src/main/java/com/grades/controller/UploadController.java
@@ -1,34 +1,105 @@
 package com.grades.controller;
 
 
-import com.grades.service.UploadService;
+import com.grades.model.*;
+import com.grades.serviceImpl.UploadServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 
 
 @Controller
+@SessionAttributes({"user"})
 @RequestMapping(value = "/GradesSystem")
 public class UploadController {
 
-    private UploadService uploadService;
+    private UploadServiceImpl uploadServiceImpl;
 
     @Autowired
-    public UploadController(UploadService uploadService){
-        this.uploadService = uploadService;
+    public UploadController(UploadServiceImpl uploadServiceImpl){
+        this.uploadServiceImpl = uploadServiceImpl;
     }
 
-    @RequestMapping(value = "/upload.do")
-    public ModelAndView doUpload(@RequestParam("uploadFile") CommonsMultipartFile file, HttpServletRequest request){
-        ModelAndView mav = new ModelAndView();
-        String result = uploadService.upload(file,request);
-        mav.addObject("error",result);
-        mav.setViewName("error");
-        return mav;
+    /**
+     *检测表名是否可用
+     * @param tableName
+     * @param request
+     * @return {"isAvailable":"false/true"}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/checkTableName")
+    public String checkTableName(String tableName,HttpServletRequest request){
+        User user = (User) request.getSession().getAttribute("user");
+        String result = uploadServiceImpl.checkTableName(user.getId()+"_"+tableName);
+        return result;
     }
+
+    /**
+     * 文件上传
+     * @param file
+     * @param request
+     * @return {"uploadResult:"false/true"}
+     */
+    @ResponseBody
+    @RequestMapping(value = "/upload")
+    public String doUpload(@RequestParam("uploadFile") CommonsMultipartFile file, HttpServletRequest request){
+        String result = uploadServiceImpl.upload(file,request);
+        return result;
+    }
+
+    /**
+     * 读取文件内容
+     * @return {"readResult":"false/true"}
+     */
+    @ResponseBody
+    @RequestMapping(value = "fileRead")
+    public String readExcel(){
+        String result = null;
+        try {
+            result = uploadServiceImpl.fileRead(uploadServiceImpl.getFilePath());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    /**
+     * 读取进度
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/readProgress")
+    public float getReadProgress(){
+        return uploadServiceImpl.getReadProgress();
+    }
+
+    /**
+     * 将文件内容写入数据库
+     * @param request
+     * @return "{"createResult":"false/true","insertResult":"false/true","updateTableInfoResult":"false/true"}
+     */
+    @ResponseBody
+    @RequestMapping(value = "fileWrite")
+    public String writeToDb(HttpServletRequest request){
+        User user = (User)request.getSession().getAttribute("user");
+        String result = uploadServiceImpl.fileWrite(user.getId(),uploadServiceImpl.getTableName(),uploadServiceImpl.getReadResultList());
+        return result;
+    }
+
+    /**
+     * 写入进度
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "writeProgress")
+    public float getWriteProgress(){
+        return uploadServiceImpl.getWriteProgress();
+    }
+
 }
--- src/main/java/com/grades/serviceImpl/UploadServiceImpl.java
+++ src/main/java/com/grades/serviceImpl/UploadServiceImpl.java
@@ -19,8 +19,11 @@ import java.util.List;
 @Service
 public class UploadServiceImpl implements UploadService {
 
-    private static List<List<String>> readResultList;
-    private static int readResultRows = 0;
+    private String filePath;//文件保存位置
+    private String tableName;//设置的表名
+    private List<List<String>> readResultList;//读取结果
+    private int readResultRows = 0;//总行数
+    private int writeProgress = 0;//写入进度
 
     private TableInfoMapper tableInfoMapper;
 
@@ -28,8 +31,22 @@ public class UploadServiceImpl implements UploadService {
         return readResultList;
     }
 
-    public int getReadResultRows() {
-        return readResultRows;
+    public String getTableName(){
+        return tableName;
+    }
+
+    public String getFilePath(){
+        return filePath;
+    }
+
+    public float getReadProgress(){
+        float result = (float)readResultRows/(float)readResultList.size();
+        return result;
+    }
+
+    public float getWriteProgress(){
+        float result = (float)writeProgress/(float)readResultList.size();
+        return result;
     }
 
     @Autowired
@@ -37,6 +54,12 @@ public class UploadServiceImpl implements UploadService {
         this.tableInfoMapper = tableInfoMapper;
     }
 
+    /**
+     * 文件上传
+     * @param file
+     * @param request
+     * @return {"uploadResult:"false/true"}
+     */
     public String upload(CommonsMultipartFile file, HttpServletRequest request) {
         if (!file.isEmpty() && request != null){
             //获取文件类型
@@ -51,54 +74,81 @@ public class UploadServiceImpl implements UploadService {
             } catch (IOException e) {
                 e.printStackTrace();
             }
-            return "{\"uploadResult:\"true\",\"path\":"+path+fileName+"\"}";
+            filePath = path+fileName;
+            return "{\"uploadResult:\"true\"";
         }
-        return "{\"uploadResult:\"false\",\"path\":"+null+"\"}";
+        return "{\"uploadResult:\"false\"}";
     }
 
+    /**
+     * 检测表名是否可用
+     * @param tableName
+     * @return {"isAvailable":"false/true"}
+     */
     public String checkTableName(String tableName) {
         String result = null;
         if (tableName != null && !tableName.trim().equals("")){
             result = tableInfoMapper.findTable(tableName);
         }
-        return result;
+        if (result != null){
+            this.tableName = tableName;
+            return "{\"isAvailable\":\"true\"}";
+        }
+        return "{\"isAvailable\":\"false\"}";
     }
 
+    /**
+     * 读取文件内容，同时获取总行数
+     * @param fileName
+     * @return {"readResult":"false/true"}
+     * @throws Exception
+     */
     public String fileRead(String fileName) throws Exception {
         //获取工作表
         Workbook wb = ExcelReader.getWorkbook(fileName);
         //获取工作簿
         Sheet sheet = ExcelReader.getSheet(wb,0);
+        //获取总行数
+        readResultRows = ExcelReader.getRowNumber(sheet);
         //读取数据
         readResultList = ExcelReader.getExcelRows(sheet,-1,-1);
-        if (readResultList != null){
-            readResultRows = readResultList.size();
-            return "{\"readResult\":\"true\",\"rows\":\""+readResultRows+"\"}";
+        if (readResultList.size() == readResultRows){
+            return "{\"readResult\":\"true\"}";
         }
-        return "{\"readResult\":\"false\",\"rows\":\""+readResultRows+"\"}";
+        return "{\"readResult\":\"false\"}";
     }
 
+    /**
+     * excel写入数据库,写入进度
+     * @param userId
+     * @param tableName
+     * @param lists
+     * @return "{"createResult":"false/true","insertResult":"false/true","updateTableInfoResult":"false/true"}
+     */
     public String fileWrite(int userId, String tableName, List<List<String>> lists) {
+        int groupSize = 20;//分组大小
+        String tempTableName = userId+"_"+tableName;
         boolean createResult;
         boolean insertResult = false;
         boolean updateTableInfoResult = false;
         //创建表
         List<String> listHead = lists.get(0);
-        createResult = tableInfoMapper.createTable(tableName , listHead);
+        createResult = tableInfoMapper.createTable(tempTableName, listHead);
         if (createResult){
             //去掉头部-列名
             lists.remove(0);
-            //分组插入数据，每组20条
-            for (int i = 0 ; i < lists.size(); i+= 20){
+            //分组插入数据
+            for (int i = 0 ; i < lists.size(); i+= groupSize){
                 List<List<String>> temp = new ArrayList<List<String>>();
                 //最后不满20条的情况
-                if (i + 20 > lists.size()){
-                    int remain = lists.size() % 20;
+                if (i + groupSize > lists.size()){
+                    int remain = lists.size() % groupSize;
                     temp.addAll(lists.subList(lists.size() - remain,lists.size()));
                 }else {
-                    temp.addAll(lists.subList(i,i + 20));
+                    temp.addAll(lists.subList(i,i + groupSize));
                 }
-                insertResult = tableInfoMapper.insertData("test", temp);
+                insertResult = tableInfoMapper.insertData(tempTableName, temp);
+                writeProgress += groupSize;
                 if (!insertResult){
                     break;
                 }
@@ -108,6 +158,7 @@ public class UploadServiceImpl implements UploadService {
             //表信息更新
             updateTableInfoResult = tableInfoMapper.insertTableInfo(new TableInfo(0, tableName, userId, 0));
         }
+        writeProgress = 0;
         return "{\"createResult\":\""+createResult+"\",\"insertResult\":\""+insertResult+"\",\"updateTableInfoResult\":\""+updateTableInfoResult+"\"}";
     }
 }
