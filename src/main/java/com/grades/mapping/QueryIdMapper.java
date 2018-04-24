3603d9beb2aff426bcbb581a48f5e47403f8f9e5
JokerAlex <alex.zhao1023@gmail.com>
2018-04-24 18:28:07 +0800

    修正

--- src/main/java/com/grades/mapping/QueryIdMapper.java
+++ src/main/java/com/grades/mapping/QueryIdMapper.java
@@ -10,16 +10,16 @@ public interface QueryIdMapper {
     //query_id表
     List<QueryRecord> getRecordIdAndTables(int userId);
 
-    int getRecordId(@Param("qeuryIdName") String qeuryIdName, @Param("userId") int userId);
+    int[] getRecordId(@Param("queryIdName") String queryIdName, @Param("userId") int userId);
 
     boolean insertRecordId(@Param("queryIdName") String queryIdName, @Param("userId") int userId);
 
-    boolean delQueryId(@Param("queryId") int queryId, @Param("userId") int userId);
+    boolean delQueryId(@Param("list") List list, @Param("userId") int userId);
 
     //query_tables表
     boolean insertIdAndTables(@Param("queryId") int queryId, @Param("list") List tableIdList, @Param("status") String status);
 
-    boolean delIdAndTables(int queryId);
+    boolean delIdAndTables(List queryId);
 
     boolean updateTableStatus(@Param("status") String status, @Param("tableId") int tableId);
 
