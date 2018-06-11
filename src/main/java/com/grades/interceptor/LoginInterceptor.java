fde39c938748d51c10059e6fc6e1afaae306ec81
JokerAlex <alex.zhao1023@gmail.com>
2018-06-11 17:22:06 +0800

    修正

--- src/main/java/com/grades/interceptor/LoginInterceptor.java
+++ src/main/java/com/grades/interceptor/LoginInterceptor.java
@@ -1,21 +1,15 @@
 package com.grades.interceptor;
 
 import com.grades.model.User;
-import com.grades.utils.GetUser;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 public class LoginInterceptor implements HandlerInterceptor {
     public boolean preHandle(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o) throws Exception {
         //获取session
-        //String sessionId = GetUser.getUser(httpServletRequest.getCookies());
-        //System.out.println("intereceptor--------->sessionId---->"+sessionId);
-        //HttpSession session = httpServletRequest.getSession();
         User user = (User) httpServletRequest.getSession().getAttribute(httpServletRequest.getSession().getId());
         if (user != null){
             return true;
